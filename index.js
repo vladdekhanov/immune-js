26f613a6d218e889df736199cb8c453ebee0cbe9
dekhanov-vladislav-yumasoft <vladislav.dehanov@yumasoft.com>
2017-08-22 17:45:40 +0300

    support of rightJoin throw second argument with optional condition callback

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
-module.exports = function(object) {
-    if (typeof object !== 'object') return object;
-    return proxying(object);
+module.exports = function(object1, object2, compCondition) {
+    if (typeof object1 !== 'object') return object1;
+    if (!object2) return proxying(object1);
+    if (!!object2 && typeof object2 !== 'object') return proxying(object1);
+    if(!!object1 && !!object2 && typeof object1 === 'object' && typeof object2 === 'object') {
+        return rightJoin(object1, object2, compCondition)
+    }
+    return proxying(object1);
 }
+
+function rightJoin(object1, object2, compCondition) {
+    var result = object1;
+    var compare = typeof compCondition === 'function' ? compCondition : objectsAreSame
+    if (Array.isArray(object1) && Array.isArray(object2)) {
+        var result = [];
+        if (object1.length) {
+            object2.forEach((b) => {
+                object1.forEach((a) => {
+                    if (compare(a, b)) { 
+                        result.push(b); 
+                    } else {
+                        result.push(a);
+                    }
+                });
+            });
+        } else {
+            result = object2;
+        }
+    } else if(typeof object1 === 'object' && typeof object2 === 'object') {
+        if (compare(object1, object2)) result = object2;
+    };
+
+    return proxying(result);
+};
 
 function proxying(object) {
     return new Proxy(object, { 
@@ -18,3 +48,18 @@ function proxying(object) {
         }
     });
 };
+
+function objectsAreSame(object1, object2) {
+    var result = true;
+    for(var propertyName in object1) {
+        if (!object1.hasOwnPropery(propertyName) || !object2.hasOwnPropery(propertyName)) {
+            result = false;
+            break;
+        }
+        if(object1[propertyName] !== object2[propertyName]) {
+            result = false;
+            break;
+        }
+    }
+    return result;
+ }
